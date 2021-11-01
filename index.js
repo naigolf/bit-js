d9abac513065f6ea597c597202ccee23052ac109
nai golf <niy_golf@hotmail.com>
2021-11-01 23:45:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,12 @@ const data = {
     
     
     
-    client.market_place_bid(data).then( => {
+    client.market_place_bid(parameters: {
+    sym: 'THB_BTC'
+    amt: 10,
+    rat: 260000
+    typ: 'limit'
+  }).then( market_place_bid => {
 	    
 	    
   //console.log("bit"+req.body)
